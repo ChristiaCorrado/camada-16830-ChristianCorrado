82e63f9d9385234b4c3925a76922fddfe03a2cb3
ChristiaCorrado <86751141+ChristiaCorrado@users.noreply.github.com>
2021-09-05 20:57:31 -0300

    commitInicial

--- script/crearTareas.js
+++ script/crearTareas.js
@@ -1,8 +1,13 @@
 const getDataTarea = () => {
+
+    if (document.getElementById("nuevaTarea").value === '') {
+        console.log('vacio')
+        return
+    }else{
+
     return tarea = document.getElementById("nuevaTarea").value;
-
+    }
 }
-
 
 
 
@@ -10,18 +15,27 @@ const clickNuevaTarea = document.getElementById("agregar")
 
 clickNuevaTarea.addEventListener('click', ()=>{
 
-    // let inputTarea = getDataTarea();
-
-    let txt = document.getElementById("listaTarea");
-
     
     listaTarea.push(getDataTarea());
-    
-    listaTarea.forEach(element => {
 
-        txt.innerHTML = `<p> ${element} </p>`
+    const tareaAgendada = document.getElementById("listaTarea")
+
+    let acumulador = ``;
+    listaTarea.forEach(e => {
+
+        acumulador+= `<div  class="alert alert-warning  d-flex justify-content-between align-items-center">
+
+        <p class="m-0">Tarea ${e} </p>
+        <div class=" m-0 d-flex align-item-center ">
+          <i class="fas fa-check text-success p-1"></i>
+          <i class="fas fa-times text-danger p-1"></i>
+        </div>
+        </div>`
         
     });
+                
+    tareaAgendada.innerHTML = acumulador;
+  
 })
 
 
